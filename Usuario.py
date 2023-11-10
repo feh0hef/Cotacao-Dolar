f415f40807539039bb570b3b367590a90161ee90
Feh <111074576+feh0hef@users.noreply.github.com>
2023-11-10 14:47:17 -0300

    Update Usuario.py

--- Usuario.py
+++ Usuario.py
@@ -6,10 +6,18 @@ def solicitar_usuario():
 
     print("----------------------")
     print("Informe a data de cotação desejada:")
-    mes = input("informe o mes desejado:")
-    dia = input("informe o dia desejado:")
-    ano = input("informe o ano desejado:")
+    mes = int(input("informe o mes desejado:"))
+    dia = int(input("informe o dia desejado:"))
+    ano = int(input("informe o ano desejado:"))
     print("----------------------")
-    data = '{}-{}-{}'.format(mes, dia, ano)
-    data = datetime.strptime(data, '%m-%d-%Y').date()
-    return data
+
+    if dia < 10 and mes < 10:
+        data_bd = '{}-0{}-0{}'.format(ano, mes, dia)
+    elif dia < 10 and mes >= 10:
+        data_bd = '{}-{}-0{}'.format(ano, mes, dia)
+    elif dia >= 10 and mes < 10:
+        data_bd = '{}-0{}-{}'.format(ano, mes, dia)
+    else:
+        data_bd = '{}-{}-{}'.format(ano, mes, dia)
+
+    return data_bd
